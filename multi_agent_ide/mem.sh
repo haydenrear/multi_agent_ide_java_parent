69a756d268e2ea6b6e8e6bddbe5ca0364feefd69
hayden.rear <hayden.rear@gmail.com>
2026-02-06 01:09:18 -0500

    adding script for starting mem easily

--- multi_agent_ide/mem.sh
+++ multi_agent_ide/mem.sh
@@ -1,10 +1,3 @@
-export HINDSIGHT_API_LLM_PROVIDER=ollama
-export HINDSIGHT_API_LLM_BASE_URL=http://localhost:11434/v1
-export HINDSIGHT_API_LLM_MODEL=llama3
-
-docker run --rm -it --pull always -p 8888:8888 -p 9999:9999 \
-  -e HINDSIGHT_API_LLM_API_KEY=$OPENAI_API_KEY \
-  -e HINDSIGHT_API_LLM_BASE_URL=http://localhost:11434/v1 \
-  -e HINDSIGHT_API_LLM_MODEL=llama3 \
-  -v $HOME/.hindsight-docker:/home/hindsight/.pg0 \
-  ghcr.io/vectorize-io/hindsight:latest
+cd src/main/resources/docker
+ollama serve &
+docker-compose -f docker-compose-hindsight.yml
