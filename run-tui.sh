b5aae269690996e257c8cf404287a2bcac619e13
hayden.rear <hayden.rear@gmail.com>
2026-02-10 23:49:55 -0500

    random commits for fix later

--- run-tui.sh
+++ run-tui.sh
@@ -1,4 +1,4 @@
 cd multi_agent_ide
 ./gradlew bootJar
 cd ..
-SPRING_PROFILES_ACTIVE=codex,cli java -jar multi_agent_ide/build/libs/multi_agent_ide-0.0.1-SNAPSHOT.jar tui
+SPRING_PROFILES_ACTIVE=goose,cli java -jar multi_agent_ide/build/libs/multi_agent_ide-0.0.1-SNAPSHOT.jar tui
