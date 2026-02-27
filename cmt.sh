cde189327f2c37c4af10bfd9f0d8787af78b81aa
hayden.rear <hayden.rear@gmail.com>
2026-02-26 21:18:16 -0500

    fix openrouter free models

--- cmt.sh
+++ cmt.sh
@@ -1,24 +1,31 @@
 a=$1
 echo "Committing with $a"
+branch=$2
+
 
 git a
 git commit -m "$a" || true
+git push origin $2
 
 cd acp-cdc-ai
 git a
 git commit -m "$a" || true
+git push origin $2
 
 cd ../multi_agent_ide
 git a
 git commit -m "$a" || true
+git push origin $2
 
 cd ../multi_agent_ide_lib
 git a
 git commit -m "$a" || true
+git push origin $2
 
 cd ../utilitymodule
 git a
 git commit -m "$a" || true
+git push origin $2
 
 echo "Done committing $a"
 
